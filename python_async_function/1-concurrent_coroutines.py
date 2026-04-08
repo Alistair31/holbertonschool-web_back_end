df9427842f99f9be5389af2760829519eeec2b72
Alistair31 <gabikill@hotmail.fr>
2026-04-08 15:37:47 +0200

    feat: Add docstring to wait_n function for clarity

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
+    """Spawn wait_random n times and return delays in ascending order."""
     delays = []
     coroutines = [wait_random(max_delay) for _ in range(n)]
     for coro in asyncio.as_completed(coroutines):
