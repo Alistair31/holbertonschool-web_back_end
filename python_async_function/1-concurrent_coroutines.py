46f9b30ad6fe5138fd70183917f685215c1b8c7b
Alistair31 <gabikill@hotmail.fr>
2026-04-08 15:30:04 +0200

    refactor: Improve readability by separating coroutine creation in wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     delays = []
-    for coro in asyncio.as_completed([wait_random(max_delay) for _ in range(n)]):
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    for coro in asyncio.as_completed(coroutines):
         delays.append(await coro)
     return delays
