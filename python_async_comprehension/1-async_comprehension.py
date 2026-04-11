816513886001e8ae8c41788d361e962b129cad65
Alistair31 <gabikill@hotmail.fr>
2026-04-11 22:47:11 +0200

    fix: Correct import statement for async_generator in async_comprehension module

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for creating tasks with async comprehensions."""
 import asyncio
-async_generator = __import__('0-async_generator.py').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
