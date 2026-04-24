43c0bcc949c97083fa6bd066c2a7455ad25502fb
Alistair31 <gabikill@hotmail.fr>
2026-04-24 21:29:11 +0200

    fix: Remove unnecessary blank line in update_topics function

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,7 +4,6 @@
 
 def update_topics(mongo_collection, name, topics):
     """ update topics for a school document based on the name """
-
     doc_update = mongo_collection.update_many({'name': name},
                                               {'$set': {'topics': topics}})
     return doc_update
