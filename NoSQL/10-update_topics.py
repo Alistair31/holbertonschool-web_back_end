558958e1aff5136a4511281b473fe8d3ff9b03dc
Alistair31 <gabikill@hotmail.fr>
2026-04-24 16:48:07 +0200

    fix: Implement update_topics function to modify school topics in MongoDB

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" 10-update_topics """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ update topics for a school document based on the name """
+
+    doc_update = mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
+    return doc_update
