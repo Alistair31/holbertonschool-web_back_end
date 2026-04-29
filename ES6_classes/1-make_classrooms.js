f1286a1d5aea9f9ec7f8773a478769d6e583acad
Alistair31 <gabikill@hotmail.fr>
2026-04-29 09:25:12 +0200

    feat: Implement initializeRooms function to create classroom instances

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from "./0-classroom.js";
+
+export default function initializeRooms(ClassRoom) {
+  return ["ClassRoom { _maxStudentsSize: 19}",
+	"ClassRoom { _maxStudentsSize: 20}",
+	"ClassRoom { _maxStudentsSize: 34}"
+  ]
+}
