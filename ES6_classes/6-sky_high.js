d03bc2eae5cfd7758d42f3e2d1fec51a0cd95414
Alistair31 <gabikill@hotmail.fr>
2026-04-30 12:35:16 +0200

    feat: Implement SkyHighBuilding class with sqft and floors properties, including validation and evacuation warning message

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,31 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+	constructor(sqft, floors) {
+		super(sqft);
+		this.floors = floors;
+	}
+	
+	get sqft() {
+		return this._sqft;
+	}
+	get floors() {
+		return this._floors;
+	}
+
+	set sqft(value) {
+		if (typeof value !== 'number') {
+			throw new TypeError('Sqft must be a Number');
+		}
+		this._sqft = value;
+	}
+	set floors(value) {
+			if (typeof value !== 'number') {
+			throw new TypeError('Floors must be a Number');
+		}
+		this._sqft = value;
+	}
+	evacuationWarningMessage() {
+		return `Evacuate slowly the ${this._floors} floors`
+	}
+}
