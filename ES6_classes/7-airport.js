e6131c47135e8ddf2ce8c5afbd455b3a6e1ef520
Alistair31 <gabikill@hotmail.fr>
2026-04-30 13:44:41 +0200

    feat: Implement Airport class with name and code properties, including validation

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,26 @@
+export default class Airport {
+	constructor(name, code) {
+		this.name = name;
+		this.code = code;
+	}
+
+	get name() {
+		return this._name;
+	}
+	get code() {
+		return this._code;
+	}
+
+	set name(value) {
+		if (typeof(value) !== 'string') {
+			throw new TypeError('Name must be a String');
+		}
+		this._name = value;
+	}
+	set code(value) {
+		if (typeof(value) !== 'string') {
+			throw new TypeError('code must be a String');
+		}
+		this._code = value;
+	}
+}
