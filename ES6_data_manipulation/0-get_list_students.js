1c4e96e565e420145aa50c605bdfde506457cd7d
Alistair31 <gabikill@hotmail.fr>
2026-05-04 16:53:03 +0200

    feat: Add getListStudents function to return a list of student objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+	const students1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+	const students2 = { id: 2, firstName: 'James', location: 'Columbia' };
+	const students3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+	return [students1, students2, students3]
+}
