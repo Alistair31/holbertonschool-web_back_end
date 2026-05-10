1992fe45f00b9ef73b9e894027b72ef2c1b724a8
Alistair31 <gabikill@hotmail.fr>
2026-05-10 15:53:09 +0200

    feat: Add getStudentIdsSum function to calculate the sum of student IDs

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+import getListStudents from "./0-get_list_students.js";
+import getStudentsByLocation from "./2-get_students_by_loc.js";
+
+export default function getStudentIdsSum(students) {
+	return students.reduce((sum, student) => sum + student.id, 0);
+}
