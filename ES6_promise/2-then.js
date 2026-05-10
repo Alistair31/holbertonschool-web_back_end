180c2f279e9eb37ef5e23f02479e5d01582f8da8
Alistair31 <gabikill@hotmail.fr>
2026-05-10 19:24:41 +0200

    fix: Update object key format in handleResponseFromAPI function

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
 	return promise.then((response) => {
 		console.log("Got response from the API");
-		return { status: 200, body: "Success" };
+		return { "status": 200, body: "Success" };
 	}).catch((error) => {
 		return new Error({});
 	});
