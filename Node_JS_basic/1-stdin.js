83a2983e3e5f79b8f258c7355a4e28d06e670948
Alistair31 <gabikill@hotmail.fr>
2026-05-11 15:29:52 +0200

    fix: Correct formatting and structure of readline close event listener

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,11 +3,12 @@ const { stdin: input, stdout: output } = require('node:process');
 
 const rl = readline.createInterface({ input, output });
 
-rl.question('Welcome to Holberton School, what is your name?\n',  (answer) => {
-  console.log(`Your name is: ${answer}`)
+rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
+  console.log(`Your name is: ${answer}`);
   rl.close();
 });
- {
-	rl.on('close', (stdout) => {if (process.stdin.isTTY !== true) {
-		(console.log("This important software is now closing"))}});
-}
+rl.on('close', () => {
+  if (process.stdin.isTTY !== true) {
+    (console.log('This important software is now closing'));
+  }
+});
