c59dd84312039e147c4203f4f06bf4e642d4a8e4
Alistair31 <gabikill@hotmail.fr>
2026-05-13 16:30:34 +0200

    feat: Create HTTP server to serve student data and greeting message

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    const readfile = process.argv[2];
+    let output = 'This is the list of our students\n';
+    const originalLog = console.log;
+    console.log = (msg) => { output += `${msg}\n`; };
+
+    countStudents(readfile)
+      .then(() => {
+        console.log = originalLog;
+        res.end(output);
+      })
+      .catch((err) => {
+        console.log = originalLog;
+        res.end(err.message);
+      });
+  }
+});
+app.listen(1245, 'localhost', () => {
+  console.log(`Server running at http://localhost:${1245}/`);
+});
+
+module.exports = app;
